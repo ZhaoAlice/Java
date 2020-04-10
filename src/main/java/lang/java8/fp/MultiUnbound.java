bb4713f6d607e1abe95fb0cc5e02445321640dd5
ZLG_9101 <0027009101@iwhalecloud.com>
2020-04-10 09:07:34 +0800

    未绑定的方法引用与构造函数引用

--- src/main/java/lang/java8/fp/MultiUnbound.java
+++ src/main/java/lang/java8/fp/MultiUnbound.java
@@ -8,6 +8,20 @@ package lang.java8.fp;
  * @since 1.0.0
  */
 class MultiParam {
+    private int ctor;
+    String s;
+    MultiParam() {
+        this.ctor = 1;
+    }
+    MultiParam(int i) {
+        this.ctor = i;
+    }
+
+    MultiParam(int i, String s) {
+        this.ctor = i;
+        this.s = s;
+    }
+
     void twoParam(int i, double d) {
         System.out.println("two params");
     }
@@ -20,6 +34,15 @@ class MultiParam {
     }
 }
 
+interface Ctor0 {
+    MultiParam ctor0();
+}
+interface Ctor1 {
+    MultiParam ctor1(int i);
+}
+interface Ctor2 {
+    MultiParam ctor2(int i, String s);
+}
 interface Call2 {
     void call2(MultiParam multiParam, int i, double d);
 }
@@ -42,6 +65,17 @@ public class MultiUnbound {
         call3.call3(mt, 1,2.0, "3");
         call4.call4(mt, 1,2.0, "4", 'c');
 
+        // 编译器会根据构造函数与接口方法中的参数将正确的方法进行绑定
+        Ctor0 ctor0 = MultiParam::new;
+        Ctor1 ctor1 = MultiParam::new;
+        Ctor2 ctor2 = MultiParam::new;
+
+        // 利用构造函数引用来创建对象
+        MultiParam param0 =  ctor0.ctor0();
+
+        MultiParam param1 =  ctor1.ctor1(1);
+
+        MultiParam param2 =  ctor2.ctor2(1, "this is me");
 
     }
 }
