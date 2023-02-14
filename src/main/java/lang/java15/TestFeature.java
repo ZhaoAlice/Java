cd6e8f0ecbf4f9a352435fd0bbf62a71900a4b7e
0027009101 <zhao.lingge@iwhalecloud.com>
2023-02-14 17:36:43 +0800

    update

--- src/main/java/lang/java15/TestFeature.java
+++ src/main/java/lang/java15/TestFeature.java
@@ -11,8 +11,14 @@ import org.junit.Test;
  */
 public class TestFeature {
 
+    private String name;
+
     @Test
-    public void testTextBlock() {
+    public void testTextBlock(Object e) {
+        // 判断实例的类型时，直接创建临时变量，便于后面引用，不需要再进行强制转换
+        if (e instanceof lang.java15.TestFeature testFeature) {
+            System.out.println(testFeature.name);
+        }
 
     }
 }
